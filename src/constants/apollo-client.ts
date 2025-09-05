97c503e496926cd37b049e57cb58202553511f3a
MatheusTrinca <matheusfelipetrinca@gmail.com>
2025-09-05 00:30:53 +0000

    feat: checking-unauthorized

--- src/constants/apollo-client.ts
+++ src/constants/apollo-client.ts
@@ -1,13 +1,28 @@
 import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import { onError } from '@apollo/client/link/error';
+import excludedRoutes from './excluded-routes';
+import router from '../components/Routes';
+
+const logoutLink = onError(error => {
+  if (
+    error.graphQLErrors?.length &&
+    (error.graphQLErrors[0].extensions?.originalError as any).statusCode === 401
+  ) {
+    if (!excludedRoutes.includes(window.location.pathname)) {
+      router.navigate('/login');
+      client.resetStore();
+    }
+  }
+});
 
 const httpLink = createHttpLink({
   uri: '/graphql',
   credentials: 'include',
 });
 
-const apolloClient = new ApolloClient({
-  link: httpLink,
+const client = new ApolloClient({
+  link: logoutLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
-export default apolloClient;
+export default client;
